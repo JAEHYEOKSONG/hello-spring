4f7a18706cc44139d17daa58fe7fbc4cd31e7bfa
JHEHYEOKSONG <jhmo97@naver.com>
2022-01-08 23:52:22 +0900

    업데이트

--- src/test/java/com/example/hellospring/service/MemberServiceTest.java
+++ src/test/java/com/example/hellospring/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import com.example.hellospring.domain.Member;
 import com.example.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,10 +12,16 @@ import static  org.assertj.core.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
 
-    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    MemoryMemberRepository memberRepository;
 
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
     @AfterEach
     public void afterEach(){ //순서 상관없게 초기화 해주는 메소드
         memberRepository.clearStore();
@@ -39,14 +46,13 @@ class MemberServiceTest {
         //given
         Member member1 = new Member();
         member1.setName("spring");
-
         Member member2 = new Member();
         member2.setName("spring");
         //when
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () ->memberService.join(member2)); //예외가 나옴
-
-        assertThat(e.getMessage()).isEqualTo("이미존재하는 회원 입니다.");
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 //        try {
 //            memberService.join(member2);
 //            fail();
@@ -55,6 +61,10 @@ class MemberServiceTest {
 //        }
         //then
     }
+
+
+
+
     @Test
     void findMembers() {
     }
